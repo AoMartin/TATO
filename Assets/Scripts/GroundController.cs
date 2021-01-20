c2981ba7002ee861961831616fdb14a2a9040113
Martin Ao <martin.andres.olsson@gmail.com>
2021-01-19 22:33:56 -0300

    agregando archivos para probar el terreno destrucitble, todavia tiene bugs

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GroundController : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+	private float widthWorld, heightWorld;
+	private int widthPixel, heightPixel;
+	private Color transparent; 
+	
+	//Se usa para ir calculando los valores de Y en el borde del circulo y guardarlos para no tener que recarcularlos
+	Dictionary<int,int> coordYinCircle_cache = new Dictionary<int, int>();
+
+	// Start() de GroundController
+	void Start(){
+		spriteRenderer = GetComponent<SpriteRenderer>(); 
+		// sr : variavel global de GroundController, ref para o SpriteRenderer de Ground
+		//Texture2D tex = (Texture2D) Resources.Load("ground2");
+		// Resources.Load("nome_do_arquivo") carrega um arquivo localizado
+		// em Assets/Resources
+		Texture2D tex =spriteRenderer.sprite.texture;
+		Texture2D tex_clone = (Texture2D) Instantiate(tex);
+		// Criamos uma Texture2D clone de tex para nao alterarmos a imagem original 
+		spriteRenderer.sprite = Sprite.Create(tex_clone, 
+		                          new Rect(0f, 0f, tex_clone.width, tex_clone.height),
+		                          new Vector2(0.5f, 0.5f), 100f);
+		transparent = new Color(0f, 0f, 0f, 0f);
+		InitSpriteDimensions();
+	}
+
+	private void InitSpriteDimensions() {
+		widthWorld = spriteRenderer.bounds.size.x;
+		heightWorld = spriteRenderer.bounds.size.y;
+		widthPixel = spriteRenderer.sprite.texture.width;
+		heightPixel = spriteRenderer.sprite.texture.height;
+	}
+
+	private void CalculateCircleBorderCache(int radio){
+
+		int i,d;
+		for ( i = 0; i <= radio; i++)
+		{
+			// r = Sqrt(x*x + y*y) => y = Sqrt(r*r - x*x)
+			d = Mathf.RoundToInt(Mathf.Sqrt(radio * radio - i * i));
+			coordYinCircle_cache.Add(i,d);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if(other.gameObject.CompareTag("Player")){
+			DestroyGround(other.collider as CircleCollider2D);
+		}
+	}
+
+	public void  DestroyGround( CircleCollider2D collider ){
+
+		Vector2Int center = World2Pixel(collider.bounds.center.x, collider.bounds.center.y);
+		int radio = Mathf.RoundToInt(collider.bounds.size.x*widthPixel/widthWorld);
+
+		int px, nx, py, ny, bordeY;
+		
+		for (int i = 0; i <= radio; i++)
+		{
+			//Chequea si el cache de coordY no esta vacio para tomar el valor
+			if(coordYinCircle_cache.Count > 0){
+				bordeY = coordYinCircle_cache[i];
+			}
+			else{
+				CalculateCircleBorderCache((int)radio);
+				bordeY = coordYinCircle_cache[i];
+			}
+			
+			for (int j = 0; j <= bordeY; j++)
+			{
+				px = center.x + i;
+				nx = center.x - i;
+				py = center.y + j;
+				ny = center.y - j;
+
+				spriteRenderer.sprite.texture.SetPixel(px, py, transparent);
+				spriteRenderer.sprite.texture.SetPixel(nx, py, transparent);
+				spriteRenderer.sprite.texture.SetPixel(px, ny, transparent);
+				spriteRenderer.sprite.texture.SetPixel(nx, ny, transparent);
+			}
+		}
+		spriteRenderer.sprite.texture.Apply();
+
+		//"""Recalcular el collider (por ahora)"""
+		Destroy(GetComponent<PolygonCollider2D>());
+		gameObject.AddComponent<PolygonCollider2D>();
+	}
+
+	private Vector2Int World2Pixel(float x, float y) {
+		Vector2Int vector = new Vector2Int();
+		
+		float dx = x-transform.position.x;
+		vector.x = Mathf.RoundToInt(0.5f*widthPixel+ dx *widthPixel/widthWorld);
+		
+		float dy = y - transform.position.y;
+		vector.y = Mathf.RoundToInt(0.5f * heightPixel + dy * heightPixel / heightWorld);
+		
+		return vector;
+	}
+}
---
+++ Assets/Scripts/TatoMoveTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TatoMoveTest : MonoBehaviour
+{
+    float movem;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movem = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate() {
+        transform.position += Vector3.up * movem * Time.fixedDeltaTime;
+    }
+}
