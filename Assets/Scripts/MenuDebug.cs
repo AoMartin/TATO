98d128eb1664f4d7ad3ec1e6abf77929d2464398
Martin Ao <20m20martin@gmail.com>
2021-02-25 19:53:01 -0300

    menu debug

---
+++ Assets/Scripts/MenuDebug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDebug : MonoBehaviour
+{
+    bool mostrar_menu = false;
+    public KeyCode tecla;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(tecla)) mostrar_menu = !mostrar_menu;
+    }
+
+    private void OnGUI() {
+        if(!mostrar_menu) return;
+
+        
+    }
+}
