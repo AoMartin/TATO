b3201f82a2092a64d55c370856aa5f7f52a815ed
Martin Ao <20m20martin@gmail.com>
2021-09-09 19:55:45 -0300

    se reorganizan assets

---
+++ Assets/Scripts/_obsoleto/TatoMoveTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TatoMoveTest : MonoBehaviour
+{
+    protected Vector3 movem;
+    public float velocidad = 1.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        movem.y = Input.GetAxis("Vertical");
+        movem.x = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate() {
+        transform.position += movem * Time.fixedDeltaTime;
+    }
+
+}
